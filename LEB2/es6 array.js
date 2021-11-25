49ea4c7d54752829114046a9661c0ded8b9f6bd0
Puwanai3510 <630112418053@bru.ac.th>
2021-11-25 15:59:30 +0700

    LAB2

---
+++ LEB2/es6 array.js
@@ -0,0 +1,24 @@
+let cars = ["toyotr","cat","misda"]
+let fruits = []
+fruits[0]
+fruits[3]
+let rneam = new Array("P","K","T");
+console.log(rname);
+
+let popcar.log(popcar);
+console.log(popcar);
+console.log(cars);
+
+cars.unshift("KOBOKA");
+console.log( car);
+cars.shift(1, 0, "Masda", "KOBATR");
+console.log(cars);
+
+const carSlide =cars.shift(1,2);
+console.log(carslide);
+
+Const newArr = cars.concat(rname);
+console.log(newArr2)[...cars, ...rname];
+console.log(newArr2);
+
+console.log(newArr.toString());
---
+++ LEB2/es6 basic.js
@@ -0,0 +1,28 @@
+var name = "";
+sname = "vong";
+
+rname = "Protae";
+console.log(rname);
+
+{
+      let rname = "john";
+      console.log(rname);
+    
+}
+
+console.log(rname);
+
+{
+    const Number = 10;
+    //Number//
+}
+
+
+function add(a, b){
+let c = a + b;
+return c;
+
+};
+
+const addES = (a , b ) => a + b:
+console.log(return);
